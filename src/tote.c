2c5bea21aa25fac3002b5598b68f30cab1e73857
Blake Spangenberg <blake.spangenberg1@gmail.com>
2021-01-30 14:58:08 -0800

    implemented reading door preset from file

---
+++ src/tote.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "cJSON.h"
+#include "randomprime.h"
+
+#define DOORS_FILEPATH ("doors.json")
+
+static size_t get_file_size(FILE* f_ptr)
+{
+    assert(f_ptr);
+    fseek(f_ptr, 0L, SEEK_END);
+    size_t size = ftell(f_ptr);
+    fseek(f_ptr, 0, SEEK_SET);
+}
+
+/*
+ * Reads the contents of the provided file as JSON. Returns pointer to
+ * serialized json string in heap. Caller of this function is responsible
+ * for freeing returned pointer.
+ */
+static const char* read_doors_preset(const char* doors_filepath)
+{
+    FILE* doors_file = fopen(doors_filepath, "r");
+    assert(doors_file == NULL && "Failed to open doors file for reading");
+    
+    size_t size = get_file_size(doors_file);
+    char* doors_text = malloc(size+1);
+    assert(doors_text && "Falid to allocate memory for doors file content");
+    memset(doors_text, 0, size+1);
+
+    size_t read_bytes = fread(doors_text, size, 1, doors_file);
+    assert(read_bytes && "Failed to read all doors file text");
+
+    cJSON* doors_json = cJSON_Parse(doors_text);
+    assert(doors_json && "Failed to parse door preset JSON");
+
+    const char* doors_parsed = cJSON_PrintUnformatted(doors_json);
+    assert(doors_parsed && "Failed to serialize JSON");
+    
+    cJSON_Delete(doors_json);
+    fclose(doors_file);
+
+    return doors_parsed;
+}
+
+int main(void)
+{
+    const char* doors_preset = read_doors_preset(DOORS_FILEPATH);
+    printf("%s",doors_preset);
+    free(doors_preset);
+    return 0;
+}
