5e6256ff1f49a9d52619bc7d1ee653e1c0209bb2
Blake Spangenberg <bspangenberg@insteon.com>
2021-02-16 09:15:02 -0800

    added annotated maps to releases

--- tools/win/release.bat
+++ tools/win/release.bat
@@ -10,11 +10,13 @@ cd win
 mkdir metroid-prime-plandomizer
 cd metroid-prime-plandomizer
 mkdir plandos
+mkdir maps
 cd ..\..\..
 copy randomprime\target\debug\randomprime_patcher.exe release\win\metroid-prime-plandomizer\plandomizer_patcher.exe /Y
 copy README.md release\win\metroid-prime-plandomizer /Y
 copy tools\win\patch.bat release\win\metroid-prime-plandomizer  /Y
 xcopy plandos release\win\metroid-prime-plandomizer\plandos /Y /E
+xcopy maps release\win\metroid-prime-plandomizer\maps /Y /E
 cd release\win
 tar.exe -a -c -f metroid-prime-plandomizer-vX.X-windows.zip metroid-prime-plandomizer
 pause
