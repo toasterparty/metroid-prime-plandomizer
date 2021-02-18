79fd1239c3d42c263f00d40a6e7a39018f118353
Blake Spangenberg <bspangenberg@insteon.com>
2021-02-17 21:03:33 -0800

    .

--- tools/win/release.bat
+++ tools/win/release.bat
@@ -10,11 +10,13 @@ cd win
 mkdir metroid-prime-plandomizer
 cd metroid-prime-plandomizer
 mkdir doc
+mkdir plandos
 cd ..\..\..
 copy randomprime\target\debug\randomprime_patcher.exe release\win\metroid-prime-plandomizer\plandomizer_patcher.exe /Y
 copy README.md release\win\metroid-prime-plandomizer /Y
 copy tools\win\patch.bat release\win\metroid-prime-plandomizer  /Y
 xcopy doc release\win\metroid-prime-plandomizer\doc /Y /E
+xcopy plandos release\win\metroid-prime-plandomizer\plandos /Y /E
 cd release\win
 tar.exe -a -c -f metroid-prime-plandomizer-vX.X-windows.zip metroid-prime-plandomizer
 pause
